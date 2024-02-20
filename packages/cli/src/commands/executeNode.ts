5ea32148e015b570623c2de0694c059b88be1564
ozweiss <thewizarodofoz@gmail.com>
2024-02-20 15:29:42 +0200

    executeNode command - support poll

--- packages/cli/src/commands/executeNode.ts
+++ packages/cli/src/commands/executeNode.ts
@@ -1,16 +1,18 @@
 import { BaseCommand } from './BaseCommand';
 import { Args } from '@oclif/core';
 import { NodeTypes } from '@/NodeTypes';
+import { CredentialTypes } from '@/CredentialTypes';
 import { Container } from 'typedi';
-import type { INode, IWorkflowExecuteAdditionalData, ITaskDataConnections } from 'n8n-workflow';
-import { Node, Workflow } from 'n8n-workflow';
+import type {INode, IWorkflowExecuteAdditionalData, ITaskDataConnections, INodeType} from 'n8n-workflow';
+import {Node, Workflow, WorkflowActivateMode, WorkflowExecuteMode} from 'n8n-workflow';
 import * as NodeExecuteFunctions from 'n8n-core';
 
 export class ExecuteNode extends BaseCommand {
 	static strict = false;
 
 	static args = {
-		type: Args.string(),
+		nodeTypeId: Args.string(), // i.e. n8n-nodes-base.mongoDb
+		executionType: Args.string(), // execute / poll / trigger
 		parameters: Args.string(),
 		input: Args.string(),
 		credentials: Args.string(),
@@ -18,19 +20,21 @@ export class ExecuteNode extends BaseCommand {
 
 	async run() {
 		const { args } = await this.parse(ExecuteNode);
-		const type = args.type;
+		const nodeTypeId = args.nodeTypeId;
+		const executionType = args.executionType;
 		const parameters = JSON.parse(args.parameters);
 		const input = JSON.parse(args.input);
 		const credentials = JSON.parse(args.credentials);
-		this.log(`Executing node of type: ${type}`);
+		this.logger.info(`Executing node`, { nodeTypeId, executionType });
 
 		const nodeTypes = Container.get(NodeTypes);
-		const nodeType = nodeTypes.getByNameAndVersion(type);
-		const credentialsType = nodeType.description.credentials[0].name;
+		const nodeType = nodeTypes.getByNameAndVersion(nodeTypeId);
+		const credentialsType = credentials.type;
+		delete credentials.type;
 
 		const node: INode = {
 			id: '123',
-			type,
+			type: nodeTypeId,
 			parameters,
 			// we don't really need this because we mock the credentialsHelper
 			// but if we don't pass it, we get an error
@@ -47,34 +51,60 @@ export class ExecuteNode extends BaseCommand {
 			getDecrypted() {
 				return credentials;
 			},
+			getParentTypes(typeName: string) {
+				return Container.get(CredentialTypes).getParentTypes(typeName);
+			}
 		};
 
 		const additionalData: IWorkflowExecuteAdditionalData = {
 			credentialsHelper,
 		};
 
-		const context = NodeExecuteFunctions.getExecuteFunctions(
-			workflow, // workflow
-			{}, // runExecutionData
-			0, // runIndex
-			{}, // connectionInputData
-			inputData, // inputData
-			node,
-			additionalData, // additionalData,
-			{}, // executionData,
-			'cli', // mode,
-			[], // closeFunctions,
-			null, // abortSignal,
-		);
+		const workflowExecuteMode: WorkflowExecuteMode = 'manual';
+		const activation: WorkflowActivateMode = 'manual';
 
-		const data =
-			nodeType instanceof Node
-				? await nodeType.execute(context)
-				: await nodeType.execute.call(context);
+		let data;
+		let webhookData = {};
+		switch (executionType) {
+			case 'execute':
+				const executeFunctions = NodeExecuteFunctions.getExecuteFunctions(
+					workflow, // workflow
+					{}, // runExecutionData
+					0, // runIndex
+					{}, // connectionInputData
+					inputData, // inputData
+					node,
+					additionalData, // additionalData,
+					{}, // executionData,
+					workflowExecuteMode, // mode,
+					[], // closeFunctions,
+					null, // abortSignal,
+				);
 
-		this.log(`Executing node of type: ${type} success!`);
+				data = await nodeType.execute.call(executeFunctions);
+				break;
+			case 'poll':
+				const executePollFunctions = NodeExecuteFunctions.getExecutePollFunctions(
+					workflow, // Workflow,
+					node, // INode,
+					additionalData, // IWorkflowExecuteAdditionalData,
+					workflowExecuteMode, // WorkflowExecuteMode,
+					activation, // WorkflowActivateMode - don't know what this is
+				);
+				data = await nodeType.poll.call(executePollFunctions);
+				webhookData = executePollFunctions.getWorkflowStaticData('node');
+				break;
+			default:
+				throw new Error('Invalid executionType');
+		}
 
-		return JSON.stringify(data);
+		this.logger.info(`Executing node success`, { nodeTypeId, executionType });
+
+		console.log('===RESPONSE===');
+		console.log(JSON.stringify({
+			data,
+			webhookData
+		}));
 	}
 
 	async catch(error: Error) {
